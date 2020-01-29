251b64c6ec2d0d89a5d507d5a70d5625047149f1
riavalon <raven.avalon5@gmail.com>
2020-01-29 12:03:15 -0500

    move and rename files

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
+	"path/filepath"
 	"regexp"
 	"strings"
 	"time"
@@ -12,12 +14,34 @@ import (
 func main() {
 	inputDir := flag.String("target-dir", ".", "Directory where files to rename are found. Defaults to '.'")
 	flag.Parse()
-
 	files, err := ioutil.ReadDir(*inputDir)
 	if err != nil {
-		panic(err)
+		panic(fmt.Errorf("I was unable to read the directory: `%s`. I got this error:\n%v", *inputDir, err))
 	}
 
+	newDir := filepath.Join(*inputDir, "uncategorized")
+	err = os.MkdirAll(newDir, 0755)
+	if err != nil {
+		panic(fmt.Errorf("I couldn't create the directory for the non-matching files. Got this error:\n%v", err))
+	}
+
+	nonMatches := renameFiles(files)
+	moveNonMatches(nonMatches, *inputDir, newDir)
+}
+
+func moveNonMatches(files []string, inputDir, newDir string) {
+	for _, badMatch := range files {
+		oldPath := filepath.Join(inputDir, badMatch)
+		newPath := filepath.Join(newDir, badMatch)
+		err := os.Rename(oldPath, newPath)
+		if err != nil {
+			fmt.Printf("I couldn't rename this file: `%s`. Got this error:\n%v", badMatch, err)
+		}
+	}
+}
+
+func renameFiles(files []os.FileInfo) []string {
+	var nonMatches []string
 	for _, file := range files {
 		matched, err := match(file.Name())
 		if err != nil {
@@ -25,6 +49,7 @@ func main() {
 		}
 
 		if !matched {
+			nonMatches = append(nonMatches, file.Name())
 			continue
 		}
 		nameAndExt := strings.Split(file.Name(), ".")
@@ -38,6 +63,7 @@ func main() {
 		newName := fmt.Sprintf("%s--%s.%s", date.Format("January_02_2006"), strings.Split(nameAndExt[0], "_")[1], ext)
 		fmt.Println(newName)
 	}
+	return nonMatches
 }
 
 func match(filename string) (bool, error) {
@@ -46,11 +72,4 @@ func match(filename string) (bool, error) {
 		return false, err
 	}
 	return isMatch, nil
-}
-
-func parseDate(date string) (day, month, year string) {
-	year = date[0:4]
-	month = date[4:6]
-	day = date[6:]
-	return
 }
