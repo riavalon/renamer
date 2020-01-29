ea34b4adb3a75677595a3da8cb8922790f10f207
riavalon <raven.avalon5@gmail.com>
2020-01-29 13:59:25 -0500

    Add reverting from manifest file

--- main.go
+++ main.go
@@ -15,31 +15,84 @@ import (
 
 func main() {
 	inputDir := flag.String("target-dir", ".", "Directory where files to rename are found. Defaults to '.'")
+	shouldRevert := flag.Bool("revert", false, "Use manifest file in given `target-dir` to revert renaming changes.")
 	flag.Parse()
 	files, err := ioutil.ReadDir(*inputDir)
 	if err != nil {
 		panic(fmt.Errorf("I was unable to read the directory: `%s`. I got this error:\n%v", *inputDir, err))
 	}
 
-	manifestFile, err := os.Create(filepath.Join(*inputDir, "manifest.csv"))
-	if err != nil {
-		panic(fmt.Errorf("I couldn't create the manifest file. OS gave this error:\n%v", err))
-	}
+	manifestFile := getManifestFile(*shouldRevert, *inputDir)
 	defer manifestFile.Close()
-	// manifestFile := filepath.Join(*inputDir, "manifest.csv")
-	newDir := filepath.Join(*inputDir, "uncategorized")
-	err = os.MkdirAll(newDir, 0755)
-	if err != nil {
-		panic(fmt.Errorf("I couldn't create the directory for the non-matching files. Got this error:\n%v", err))
-	}
 
-	nonMatches := renameFiles(files, *inputDir, manifestFile)
-	moveNonMatches(nonMatches, *inputDir, newDir, manifestFile)
+	uncategorizedDir := filepath.Join(*inputDir, "uncategorized")
+	setUncategorizedDir(*shouldRevert, uncategorizedDir)
+
+	renameOrRevertFiles(*shouldRevert, files, *inputDir, uncategorizedDir, manifestFile)
 
 	err = manifestFile.Sync()
 	if err != nil {
 		panic(fmt.Errorf("I couldn't sync the manifest file after writing.. got this error:\n%v", err))
 	}
+}
+
+func renameOrRevertFiles(isRevertMode bool, files []os.FileInfo, inputDir, uncategorizedDir string, manifestFile *os.File) {
+	if isRevertMode {
+		records := getManifestValues(manifestFile)
+		for _, record := range records {
+			oldPath, newPath := record[0], record[1]
+			// err := os.Rename(newPath, oldPath)
+			fmt.Printf("Renaming `%s` to `%s`\n", newPath, oldPath)
+			// if err != nil {
+			// 	panic(fmt.Errorf("I ran into a problem while reverting file names.\n%v", err))
+			// }
+		}
+
+		// os.Remove(uncategorizedDir)
+		fmt.Println("Removing: ", uncategorizedDir)
+		fmt.Println("Removing: ", manifestFile.Name())
+
+		// grab oldPath and newPath
+		// perform an os.Rename from the newPath to the old Path.
+		// Clean up uncategorized folder and delete manifest file
+	} else {
+		nonMatches := renameFiles(files, inputDir, manifestFile)
+		moveNonMatches(nonMatches, inputDir, uncategorizedDir, manifestFile)
+	}
+}
+
+func getManifestValues(f *os.File) [][]string {
+	csvReader := csv.NewReader(f)
+	records, err := csvReader.ReadAll()
+	if err != nil {
+		panic(fmt.Errorf("I couldn't parse the CSV file into a 2D string slice. Got this error:\n%v", err))
+	}
+	return records
+}
+
+func setUncategorizedDir(isRevertMode bool, dirPath string) {
+	if !isRevertMode {
+		err := os.MkdirAll(dirPath, 0666)
+		if err != nil {
+			panic(fmt.Errorf("I couldn't create the directory for the non-matching files. Got this error:\n%v", err))
+		}
+	}
+}
+
+func getManifestFile(isRevertMode bool, inputDir string) *os.File {
+	manifestPath := filepath.Join(inputDir, "manifest.csv")
+	if isRevertMode {
+		file, err := os.OpenFile(manifestPath, os.O_RDWR, 0666)
+		if err != nil {
+			panic(fmt.Errorf("I ran into an error trying to open the manifest file. Here's the error:\n%v", err))
+		}
+		return file
+	}
+	file, err := os.OpenFile(manifestPath, os.O_CREATE, 0666)
+	if err != nil {
+		panic(fmt.Errorf("I ran into an error trying to create the manifest file. Here's the error:\n%v", err))
+	}
+	return file
 }
 
 func moveNonMatches(files []string, inputDir, newDir string, manifestFile io.Writer) {
@@ -52,7 +105,7 @@ func moveNonMatches(files []string, inputDir, newDir string, manifestFile io.Wri
 		if err != nil {
 			fmt.Printf("I couldn't rename this file: `%s`. Got this error:\n%v", badMatch, err)
 		}
-		csvField := []string{oldPath, newPath, "false"}
+		csvField := []string{oldPath, newPath}
 		err = csvWriter.Write(csvField)
 		if err != nil {
 			panic(fmt.Errorf("I couldn't write to the CSV file. Got this error:\n%v", err))
@@ -90,7 +143,7 @@ func renameFiles(files []os.FileInfo, inputDir string, manifestFile io.Writer) [
 		if err != nil {
 			panic(fmt.Errorf("I was unable to rename `%s`. Got this error:\n%v", file.Name(), err))
 		}
-		csvField := []string{oldPath, newPath, "true"}
+		csvField := []string{oldPath, newPath}
 		err = csvWriter.Write(csvField)
 		if err != nil {
 			panic(fmt.Errorf("I couldn't write to the CSV file. Got this error:\n%v", err))
